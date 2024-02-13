ad477f9e26ff5bb352533b754b8bc54a088bba98
patricia1691 <patrymaria16@gmail.com>
2024-02-13 21:06:16 +0200

    Add SignIn test methods

--- src/test/java/pages/BasePage.java
+++ src/test/java/pages/BasePage.java
@@ -6,13 +6,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class BasePage {
-    private static final Logger LOG= LoggerFactory.getLogger(BasePage.class);
+    private static final Logger LOG = LoggerFactory.getLogger(BasePage.class);
     public static WebDriver driver;
 
-    public static void setUp(){
+    public static void setUp() {
         LOG.info("Start test");
         System.setProperty("webdriver.chrome.driver", "C://Webdrivers/chromedriver121.exe");
-        driver=new ChromeDriver();
+        driver = new ChromeDriver();
         String url = "https://demo.automationtesting.in/Index.html";
         driver.get(url);
 
@@ -20,18 +20,18 @@ public class BasePage {
         driver.manage().window().maximize();
     }
 
-    public static void tearDown(){
+    public static void tearDown() {
         LOG.info("Close the browser");
         driver.quit();
     }
 
-    public static String returnBaseUrl(){
-        return"https://demo.automationtesting.in/Index.html";
+    public static String returnBaseUrl() {
+        return "https://demo.automationtesting.in/Index.html";
     }
 
-    public static String getBaseUrl(){
+    public static String getBaseUrl() {
         String baseurl = returnBaseUrl();
-        if(baseurl != null){
+        if (baseurl != null) {
             return baseurl.replace("Index.html", "");
         }
         return baseurl;
--- src/test/java/pages/SignInPage/SignInPage.java
+++ src/test/java/pages/SignInPage/SignInPage.java
@@ -1,38 +1,44 @@
 package pages.SignInPage;
+
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-
 import pages.BasePage;
 
 public class SignInPage extends BasePage {
-    public static final Logger  LOG = LoggerFactory.getLogger(SignInPage.class);
+    public static final Logger LOG = LoggerFactory.getLogger(SignInPage.class);
     public static SignInPage instance;
 
-    private SignInPage(){
+    private SignInPage() {
 
     }
 
-    public static SignInPage getInstance(){
-        if(instance == null){
+    public static SignInPage getInstance() {
+        if (instance == null) {
             instance = new SignInPage();
         }
         return instance;
     }
 
     private By signInButton = By.id("btn1");
+    private By logo = By.id("logo");
+    private By emailField = By.xpath(//input[@ng-model='Email']);
+    private By passwordField = By.xpath(//input[@ng-model='Password']);
 
-    public void clickSignInButton(){
+    public void clickSignInButton() {
         LOG.info("Click 'Sign in' Button");
         driver.findElement(signInButton).click();
     }
 
-    public boolean isSignInButtonDisplayed(){
+    public boolean isSignInButtonDisplayed() {
         LOG.info("Verify if 'Sign in' button is displayed");
         return driver.findElement(signInButton).isDisplayed();
+
+    }
+
+    public boolean isLogoDysplayed() {
+        LOG.info("Verify if Logo image is displayed");
+        return driver.findElement(logo).isDisplayed();
     }
 
 }
