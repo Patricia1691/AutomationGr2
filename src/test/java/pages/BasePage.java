016bc5875c8e006a0a7300114304143906a1127c
patricia1691 <patrymaria16@gmail.com>
2024-02-22 21:06:23 +0200

    Register address email and phone

--- src/test/java/pages/BasePage.java
+++ src/test/java/pages/BasePage.java
@@ -45,6 +45,6 @@ public class BasePage {
             e.printStackTrace();
         }
     }
-    }
-
 }
+
+
--- src/test/java/tests/RegisterTest/RegisterTest.java
+++ src/test/java/tests/RegisterTest/RegisterTest.java
@@ -9,18 +9,32 @@ import static pages.BasePage.driver;
 import static pages.BasePage.*;
 public class RegisterTest extends BaseTest {
     public  static final  Logger LOG = LoggerFactory.getLogger(RegisterTest.class);
-    private String newUrl= getBaseUrl() + "Register.html";
+    private String newUrl = getBaseUrl() + "Register.html";
 
     @Test
     public void fillRegister(){
         driver.get(newUrl);
+
         sleep(3000L);
+        LOG.info("Press consent button");
+        registerPage.pressConsent();
         String firstName = "Ion";
         String lastName = "Popescu";
+        String address = "Cluj, Strada Ion Barbu, nr 456";
+        String email = "automation@domnain.com";
+        String phone = "0123456789";
 
         LOG.info("Typing full name");
+        LOG.info("Type in address");
+        registerPage.insertAddress(address);
 
-        registerPage.insertFullName(firstName, lastName);
+        LOG.info("Type in email");
+        registerPage.insertEmail(email);
+
+        LOG.info("Type in phone number");
+        registerPage.insertPhoneNumber(phone);
+
+
 
 
     }
